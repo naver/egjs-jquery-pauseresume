72eda1f5beebc17c2a348c5233e166d393a5d58e
Jongmoon Yoon <jongmoon.yoon@navercorp.com>
2017-03-29 15:09:57 +0900

    chore(all): fix config error

--- karma.conf.js
+++ karma.conf.js
@@ -1,58 +1,59 @@
-var webpack = require("webpack");
-var CleanWebpackPlugin = require("clean-webpack-plugin");
-var WriteFilePlugin = require("write-file-webpack-plugin");
-var banner = require("./config/banner");
-var config = require("./config/webpack");
-var path = require("path");
+module.exports = function(config) {
+	var karmaConfig = {
+		// test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		reporters:['dots', 'progress'],
+		browsers: [],
+		frameworks: ['qunit'],
+		files: [
+			// vendor files
+			'node_modules/jquery/dist/jquery.min.js',
+			'node_modules/lite-fixture/index.js',
+			// src files
+			{pattern: "test/unit/**/*.js"}
+		],
+		webpack: {
+			devtool: 'source-map',
+			module: {
+				rules: [
+					{
+            test: /(\.js)$/,
+            exclude: /(node_modules)/,
+            loader: 'babel-loader'
+					}
+				]
+			}
+		},
+		// // preprocess matching files before serving them to the browser
+    // // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'test/unit/**/*.js': config.coverage ? ['webpack'] : ['webpack', 'sourcemap']
+    }
+	};
 
-module.exports = function(env) {
-	env = env || {};
+	if(config.coverage) {
+    karmaConfig.reporters.push("coverage");
+    karmaConfig.coverageReporter = {
+        type: 'html',
+        dir: 'coverage'
+    };
+    karmaConfig.browsers.push("PhantomJS");
+    karmaConfig.webpack.module.rules.push(
+      {
+        test: /(\.js)$/,
+        exclude: /(test|node_modules)/,
+        enforce: "pre",
+        loader: 'isparta-loader'
+      }
+    );
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    karmaConfig.singleRun = true;
+  } else {
+    karmaConfig.browsers.push("Chrome");
+    karmaConfig.singleRun = false;
+  }
 
-	if(/pkgd/.test(env.mode)) {
-		config.externals = [];
-	}
-
-	if(!/server/.test(env.mode)) {
-		config.module.rules.push({
-			test: /(\.js)$/,
-			loader: "eslint-loader",
-			include: path.resolve(process.cwd(), "src"),
-			exclude: /(node_modules)/,
-			enforce: "pre"
-		});
-	}
-
-	if(env.mode === "production") {
-		for(var p in config.entry) {
-			config.entry[p + ".min"] = config.entry[p];
-		}
-		config.plugins.push(
-			new CleanWebpackPlugin(["dist"], {
-				root: path.resolve(__dirname),
-				verbose: true,
-				dry: false
-			}),
-			new webpack.optimize.UglifyJsPlugin({
-				include: /\.min\.js$/,
-				minimize: true
-			}),
-			new webpack.BannerPlugin(banner.common)
-		);
-	} else if(env.mode === "pkgd") {
-		for(var p in config.entry) {
-			config.entry[p + ".pkgd"] = config.entry[p];
-			config.entry[p + ".pkgd.min"] = config.entry[p];
-			delete config.entry[p];
-		}
-		config.plugins.push(
-			new webpack.optimize.UglifyJsPlugin({
-				include: /\.min\.js$/,
-				minimize: true
-			}), new webpack.BannerPlugin(banner.pkgd)
-		);
-	} else {
-		config.plugins.push(new WriteFilePlugin());
-	}
-
-	return config;
+  config.set(karmaConfig);
 };
