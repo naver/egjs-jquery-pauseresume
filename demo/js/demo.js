a38ddedfe368ae6329eab1dcb92e0d9ae4176640
Jongmoon Yoon <jongmoon.yoon@navercorp.com>
2017-04-12 19:22:15 +0900

    feat(PauseResume): Add Demo

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -2,4 +2,26 @@
  * Copyright (c) NAVER Corp.
  */
 
-console.log("Hello egjs pauseresume!!!", eg.PauseResume);
+//pause / resume by mouse over/out events.
+window.onload = function() {
+	var $rectBox = $("#rectBox");
+
+	$rectBox.hover(function() {
+		$(this).pause();
+	}, function() {
+		$(this).resume();
+	});
+
+	// Animate element infinitely
+	function drawRectangle() {
+		$rectBox
+			.animate({"opacity": 0.5}, 1000)
+			.animate({"left":"+=200px"}, 1000, "easeOutElastic")//relative value
+			.animate({"top":"200px"}, 1000, "easeOutElastic")//absolute value
+			.animate({"left":"0px"}, 1000, "easeOutElastic")
+			.animate({"top":"-=200px"}, 1000, "easeOutElastic")
+			.animate({"opacity": "+=0.5"}, 1000, drawRectangle);
+	}
+
+	drawRectangle();
+}
